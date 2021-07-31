50532ee4f28db676fb3fef2ab110afca3f7b2a53
Jacob <75771725+trapzi@users.noreply.github.com>
2021-07-31 01:17:57 -0700

    Create bashography.sh

---
+++ bashography.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Enter your name:
+
+read name
+
+echo Enter your age:
+
+read age
+
+echo Enter your country:
+
+echo What are you studying:
+
+read studying
+
+echo "Hi my name Is $name, I'm $age years old currently staying at $country studying $studying!"
